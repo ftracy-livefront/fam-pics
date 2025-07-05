0e475c611e40a76a9048e9a473935b8234adb6e7
Forrest Tracy <forrest@Forrests-MacBook-Pro.local>
2025-07-05 11:04:36 -0500

    Basics of an upload button started

--- app/(tabs)/upload.tsx
+++ app/(tabs)/upload.tsx
@@ -1,5 +1,5 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, Pressable, StyleSheet, Text, View } from 'react-native';
 
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
@@ -9,13 +9,30 @@ import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Colors } from '@/constants/Colors';
 
+const onPressFunction = () => {
+    console.log("pressed")
+}
 
 export default function UploadScreen() {
     return (
         <SafeAreaProvider>
             <SafeAreaView>
                 <ThemedText type="title">Upload</ThemedText>
+                <Pressable onPress={onPressFunction}>
+                    <View
+                        style={{
+                            width: 200,
+                            height: 100,
+                            backgroundColor: Colors.livefront.lightBlue,
+                            borderRadius: 20  // Applies uniform rounded corners
+                        }}>
+                        <Text>
+                            upload an image
+                        </Text>
+                    </View>
+                </Pressable>
             </SafeAreaView>
         </SafeAreaProvider>
     )
